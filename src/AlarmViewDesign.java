d842d13e0ec132f6c75e4186ef656c6b7097dfe6
Caparoon, Matthew <mcaparo@ilstu.edu>
2017-04-11 14:28:28 -0500

    ~finished~ AlarmViewDesign...needs testing and deletion method

--- src/AlarmViewDesign.java
+++ src/AlarmViewDesign.java
@@ -1,5 +1,8 @@
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
@@ -8,7 +11,7 @@ import javax.swing.JTextArea;
  * 
  */
 
-public class AlarmViewDesign extends JPanel{
+public class AlarmViewDesign extends JPanel implements ActionListener{
 	public AlarmViewDesign(String name){
 		super(new GridLayout(1, 1));
 		JPanel avd = new JPanel();
@@ -19,11 +22,22 @@ public class AlarmViewDesign extends JPanel{
 		alarmTimeLabel.setEditable(false);
 		avd.add(alarmTimeLabel);
 		
-		
+		JButton deleteAlarmView = new JButton("X");
+		deleteAlarmView.setActionCommand("delete");
+		deleteAlarmView.addActionListener(this);
+		avd.add(deleteAlarmView);
 	}
 	
 	//this needs to be a button.
 	private void deleteAlarm(){
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if("delete".equals(e.getActionCommand())){
+			deleteAlarm();
+		}
 		
 	}
 }
