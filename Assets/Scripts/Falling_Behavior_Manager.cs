0fe22f3584993311ba1b7b391b6ff7de75011570
Reyes, Michael <mradianlogs@gmail.com>
2018-10-03 21:58:03 -0500

    Michael Reyes/Radscaryface(Discord) - I added a few scripts, Falling_Behavior and Falling_Behavior_Manager, which I am still working on.

---
+++ Assets/Scripts/Falling_Behavior_Manager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script will be in a game object whose job is to keep track of all the falling objects in the game, including randomly spawning the various kinds at the top of the screen 
+//within camera range.
+//Created by Michael Reyes, based off design by Kevin Wickart.
+public class Falling_Behavior_Manager : MonoBehaviour {
+
+    private List<List<Falling_Behavior>> masterList;
+    //Include lists of all falling objects...
+    private List<Falling_Behavior> condimentA;
+    private int numWantedCondimentsA;
+    private List<Falling_Behavior> condimentB;
+    private int numWantedCondimentsB;
+
+	// Use this for initialization
+	void Start () {
+        condimentA = new List<Falling_Behavior>();
+        condimentB = new List<Falling_Behavior>();
+        masterList = new List<List<Falling_Behavior>>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+
+/* Idea/Algorithm
+ * Go through masterList and randomly choose a type of condiment to drop.
+ *  ----In that condiment list, if the condiment's Object doesnt exist yet, create it. Else, move the invisible condiment to top of screen and set to revisible.
+ *  ~ Repeat ~
+ * 
+ */
--- Assets/Scripts/Player_Health.cs
+++ Assets/Scripts/Player_Health.cs
@@ -4,35 +4,35 @@ using UnityEngine;
 
 public class Player_Health : MonoBehaviour {
 
+	public bool hasDied;
 	public GameObject player;
-	public BoxCollider2D playerBC;
 	public GameObject camera;
 	public Vector2 startPos;
-	private bool hasDied;
-
 
 	// Use this for initialization
 	void Start () {
 		hasDied = false;
 		player = GameObject.Find("Player");
-		playerBC = player.GetComponent<BoxCollider2D>();
 		camera = GameObject.Find("Main Camera");
 		startPos = player.GetComponent<Transform>().position;
 	}
 
 	// Update is called once per frame
 	void Update () {
+		fallDeathCheck();
 		if(hasDied){
 			print("you died");
 			hasDied = false;
 		}
 	}
 
-	void OnCollisionEnter2D(Collision2D col){
-		GameObject collider = col.gameObject;
-		if(collider.tag.Equals("KillPlayer")){
-			hasDied = true;
-			player.GetComponent<Transform>().position = startPos;
-		}
+	void fallDeathCheck(){
+		//float playerHeight = player.GetComponent<Transform>().position.y;
+
+		//float cameraHeight = camera.GetComponent<Camera_System>().yMin;
+		//if(playerHeight < (cameraHeight - 4)){
+		//	hasDied = true;
+		//	player.GetComponent<Transform>().position = startPos;
+		//}
 	}
 }
