b0217bd2dcab9778cdd9d10db34dccb0db25f6b6
Joeyloveswaffles <schmidtj18@icloud.com>
2018-10-10 21:38:04 -0500

    This adds enemy movement and enemy movement with jumping added to it.

---
+++ Assets/Scripts/Enemy_Move_And_Jump.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Move_And_Jump : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	public float enemySpeed = 3f;
+	public float leftLimit = -3f;
+	public float rightLimit = 3f;
+	private float moveX;
+	private float startingPos;
+	private bool changeDirection = true;
+	public int jumpPower = 1250;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		moveX = transform.position.x;
+		startingPos = transform.position.x;
+		changeDirection = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		moveX = transform.position.x;
+		ChangeDirection();
+		JumpEnemy();
+	}
+
+	void ChangeDirection(){
+		if (changeDirection == true) {
+			if (moveX < (startingPos + rightLimit)) {
+				rb.velocity = new Vector2(enemySpeed, rb.velocity.y);
+			} else {
+				changeDirection = false;
+			}
+		} 
+
+		else {
+			if (moveX > (startingPos + leftLimit)) {
+				rb.velocity = new Vector2(enemySpeed * -1, rb.velocity.y);
+			} else {
+				changeDirection = true;
+			}
+		}
+	}
+
+	void JumpEnemy(){
+		if(rb.IsTouchingLayers(Physics2D.AllLayers)){
+			rb.AddForce(new Vector2(0, jumpPower));
+		}
+	}
+}
