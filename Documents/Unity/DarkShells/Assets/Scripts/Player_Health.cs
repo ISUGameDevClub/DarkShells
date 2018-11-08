97387f037e2e93b933442000e4705b02ec15d9e6
Chapman, Derek <dwchap1@ilstu.edu>
2018-11-07 21:53:29 -0600

    Conflict Resolution

---
+++ Documents/Unity/DarkShells/Assets/Scripts/Player_Health.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Health : MonoBehaviour {
+
+	private GameObject player;
+	private BoxCollider2D playerBC;
+	private GameObject camera;
+	private Vector2 startPos;
+	private bool hasDied;
+
+
+	// Use this for initialization
+	void Start () {
+		hasDied = false;
+		player = GameObject.Find("Player");
+		playerBC = player.GetComponent<BoxCollider2D>();
+		camera = GameObject.Find("Main Camera");
+		startPos = player.GetComponent<Transform>().position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(hasDied){
+			print("you died");
+			hasDied = false;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		GameObject collider = col.gameObject;
+		if(collider.tag.Equals("KillPlayer")){
+			hasDied = true;
+			player.GetComponent<Transform>().position = startPos;
+		}
+	}
+}
